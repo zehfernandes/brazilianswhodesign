0e2cf69273e8f4e12b22be692731fc12c91de52c
emmit-parubrub <eparubrub@dons.usfca.edu>
2021-05-16 22:08:08 -0700

    debugging deployment - previous commit still not accepting new template

--- pages/api/technologists.js
+++ pages/api/technologists.js
@@ -1,7 +1,7 @@
 import drive from "drive-db";
 
 export default async (req, res) => {
-  const db = await drive("12LLA-NoHin0zQfmpEblgMjd260bmriLMowBAH1QDOhI");
+  const db = await drive("1W7cOTU2nUfT-3N88Aa09ZtZYv_KhtYkbCtpdm_9FeT4");
   let sanitizeResult = db.filter(
     (item) => item.name != "" && item.exclude !== "Yes"
   );
