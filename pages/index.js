6815a5a48882ad45f254e5e8416caf3bcd2a6800
emmit-parubrub <eparubrub@dons.usfca.edu>
2021-05-16 22:58:15 -0700

    debugging deployment - url correction

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export async function getStaticProps() {
   const origin =
     process.env.NODE_ENV !== "production"
       ? "http://localhost:3000"
-      : "https://https://hawaiiansintech-93k9cfzcn-hawaiians.vercel.app/";
+      : "https://hawaiiansintech-93k9cfzcn-hawaiians.vercel.app";
 
   const res = await fetch(`${origin}/api/designers`);
   const designers = await res.json();
